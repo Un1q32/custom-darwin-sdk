ad46224a9663975c26fac2f89428c08d209d1cd2
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-07 10:39:37 -0500

    6th arg

--- src/syscall.c
+++ src/syscall.c
@@ -12,6 +12,7 @@ long syscall(long number, ...) {
     long arg3 = va_arg(args, long);
     long arg4 = va_arg(args, long);
     long arg5 = va_arg(args, long);
+    long arg6 = va_arg(args, long);
     va_end(args);
     asm(
         "mov r12, %[number];"
@@ -20,10 +21,11 @@ long syscall(long number, ...) {
         "mov r2, %[arg3];"
         "mov r3, %[arg4];"
         "mov r4, %[arg5];"
+        "mov r5, %[arg6];"
         "svc #0x80;"
         "mov %[ret], r0;"
         : [ret] "=r"(ret)
-        : [number] "r"(number), [arg1] "r"(arg1), [arg2] "r"(arg2), [arg3] "r"(arg3), [arg4] "r"(arg4), [arg5] "r"(arg5)
+        : [number] "r"(number), [arg1] "r"(arg1), [arg2] "r"(arg2), [arg3] "r"(arg3), [arg4] "r"(arg4), [arg5] "r"(arg5), [arg6] "r"(arg6)
     );
     return ret;
 }
