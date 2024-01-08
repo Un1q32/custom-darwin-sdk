9a35a265df307e24bcd459a9a1bb926db8cfd05a
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-08 10:07:06 -0500

    update header

--- include/sys/mman.h
+++ include/sys/mman.h
@@ -4,8 +4,12 @@
 #include <stddef.h>
 #include <sys/types.h>
 
-#define PROT_READ   0x0001
-#define PROT_WRITE  0x0002
+#define PROT_NONE   0x00
+#define PROT_READ   0x01
+#define PROT_WRITE  0x02
+#define PROT_EXEC   0x04
+
+#define MAP_SHARED  0x0001
 #define MAP_PRIVATE 0x0002
 #define MAP_ANON    0x1000
 #define MAP_ANONYMOUS MAP_ANON
