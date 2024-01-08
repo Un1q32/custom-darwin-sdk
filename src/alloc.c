36538391c4039226521118901f86321e4f2ded66
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-08 11:24:01 -0500

    add alloc functions, free doesnt work

---
+++ src/alloc.c
@@ -0,0 +1,39 @@
+#include <stddef.h>
+#include <string.h>
+#include <sys/mman.h>
+
+void free(void *ptr) {
+    if (ptr == NULL)
+        return;
+
+    munmap(ptr, 0);
+}
+
+void *malloc(size_t size) {
+    if (size == 0)
+        return NULL;
+
+    void *ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+    if (ptr == MAP_FAILED)
+        return NULL;
+
+    return ptr;
+}
+
+void *realloc(void *ptr, size_t size) {
+    if (ptr == NULL)
+        return malloc(size);
+
+    if (size == 0) {
+        free(ptr);
+        return NULL;
+    }
+
+    void *new_ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+    if (new_ptr == MAP_FAILED)
+        return NULL;
+
+    memcpy(new_ptr, ptr, size);
+    free(ptr);
+    return new_ptr;
+}
