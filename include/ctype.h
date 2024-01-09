b234bed9d3c52f54f23b9c50683ff22386a0098d
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-09 14:01:30 -0500

    add isalpha, isalnum, isxdigit

--- include/ctype.h
+++ include/ctype.h
@@ -1,11 +1,14 @@
 #ifndef _CTYPE_H_
 #define _CTYPE_H_
 
+extern int isalnum(int c);
+extern int isalpha(int c);
 extern int isascii(int c);
 extern int isblank(int c);
 extern int isdigit(int c);
 extern int islower(int c);
 extern int isspace(int c);
 extern int isupper(int c);
+extern int isxdigit(int c);
 
 #endif
--- src/ctype.c
+++ src/ctype.c
@@ -1,3 +1,23 @@
+int islower(int c) {
+    return c >= 'a' && c <= 'z';
+}
+
+int isupper(int c) {
+    return c >= 'A' && c <= 'Z';
+}
+
+int isalpha(int c) {
+    return islower(c) || isupper(c);
+}
+
+int isdigit(int c) {
+    return c >= '0' && c <= '9';
+}
+
+int isalnum(int c) {
+    return isalpha(c) || isdigit(c);
+}
+
 int isascii(int c) {
     return c >= 0 && c <= 127;
 }
@@ -6,18 +26,10 @@ int isblank(int c) {
     return c == ' ' || c == '\t';
 }
 
-int isdigit(int c) {
-    return c >= '0' && c <= '9';
-}
-
-int islower(int c) {
-    return c >= 'a' && c <= 'z';
-}
-
 int isspace(int c) {
     return c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v';
 }
 
-int isupper(int c) {
-    return c >= 'A' && c <= 'Z';
+int isxdigit(int c) {
+    return isdigit(c) || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
 }
