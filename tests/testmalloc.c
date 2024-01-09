4fa6e6d327d0d1c26e6a8dd8b81943eb3034d7eb
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-09 04:33:57 -0500

    add tests

---
+++ tests/testmalloc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char *str = malloc(4);
+    str[0] = 'a';
+    str[1] = 'b';
+    str[2] = 'c';
+    str[3] = '\0';
+    puts(str);
+    free(str);
+    return 0;
+}
