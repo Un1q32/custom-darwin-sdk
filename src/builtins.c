ab22f87005e1c63e70b66e5c122ec2cab7b9f5e3
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-10 06:49:42 -0500

    add more builtins

--- src/builtins.c
+++ src/builtins.c
@@ -13,3 +13,19 @@ int __divsi3(int a, int b) {
     }
     return ret;
 }
+
+int __udivsi3(unsigned int a, unsigned int b) {
+    int ret = 0;
+    while (a >= b) {
+        a -= b;
+        ret++;
+    }
+    return ret;
+}
+
+int __umodsi3(unsigned int a, unsigned int b) {
+    while (a >= b) {
+        a -= b;
+    }
+    return a;
+}
