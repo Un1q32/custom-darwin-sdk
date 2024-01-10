071cf52d55e6cab58dda4ea66b01141f7c638a49
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-10 10:24:47 -0500

    more builtins

--- src/builtins.c
+++ src/builtins.c
@@ -29,3 +29,35 @@ int __umodsi3(unsigned int a, unsigned int b) {
     }
     return a;
 }
+
+int __divdi3(long long a, long long b) {
+    int ret = 0;
+    while (a >= b) {
+        a -= b;
+        ret++;
+    }
+    return ret;
+}
+
+int __moddi3(long long a, long long b) {
+    while (a >= b) {
+        a -= b;
+    }
+    return a;
+}
+
+int __udivdi3(unsigned long long a, unsigned long long b) {
+    int ret = 0;
+    while (a >= b) {
+        a -= b;
+        ret++;
+    }
+    return ret;
+}
+
+int __umoddi3(unsigned long long a, unsigned long long b) {
+    while (a >= b) {
+        a -= b;
+    }
+    return a;
+}
