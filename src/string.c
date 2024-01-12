2fe4053ecbc6fb912253eff8ea4be0e323e5d620
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-12 09:34:59 -0500

    fix syntax

--- src/string.c
+++ src/string.c
@@ -12,14 +12,12 @@ char *strcat(char *dest, const char *src) {
 
 char *strcpy(char *dest, const char *src) {
     char *ret = dest;
-    while ((*dest++ = *src++) != '\0')
-        ;
+    while ((*dest++ = *src++) != '\0');
     return ret;
 }
 
 char *stpcpy(char *dest, const char *src) {
-    while ((*dest++ = *src++) != '\0')
-        ;
+    while ((*dest++ = *src++) != '\0');
     return dest - 1;
 }
 
