5a1d27d3462fd3810edf24761a9c63646b6c356b
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-12 22:58:08 -0500

    define quad_t, u_quad_t

--- include/sys/types.h
+++ include/sys/types.h
@@ -2,6 +2,8 @@
 #define _SYS_TYPES_H_
 
 typedef long ssize_t;
+typedef long long quad_t;
+typedef unsigned long long u_quad_t;
 typedef long long off_t;
 typedef off_t fpos_t;
 typedef unsigned short mode_t;
