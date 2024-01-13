3423aae007519fec3eebf207d002df33f7c54568
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-12 19:26:19 -0500

    better builtin

--- src/builtins.c
+++ src/builtins.c
@@ -1,11 +1,19 @@
 unsigned long long __divmoddi4(unsigned long long a, unsigned long long b, unsigned long long *rem) {
     unsigned long long r = 0;
-    while (a >= b) {
-        a -= b;
-        r++;
+    unsigned long long q = 0;
+    unsigned long long i = 64;
+    while (i--) {
+        r <<= 1;
+        r |= (a >> 63) & 1;
+        a <<= 1;
+        q <<= 1;
+        if (r >= b) {
+            r -= b;
+            q |= 1;
+        }
     }
-    *rem = a;
-    return r;
+    *rem = r;
+    return q;
 }
 
 unsigned long long __udivdi3(unsigned long long a, unsigned long long b) {
