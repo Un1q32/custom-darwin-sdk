699e48bb2473ee5bba07581b78e2a61b9cbeaa5f
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-24 18:00:17 -0500

    better code

--- crt/crt1.c
+++ crt/crt1.c
@@ -5,9 +5,7 @@ void _start(void) __asm__("start");
 void _start(void) {
     int argc = 0;
     char **argv = (char **)(&argc + 4);
-    char **argv_index = argv;
-    while (*argv_index != NULL)
-        argv_index++;
-    argc = argv_index - argv;
+    while (argv[argc] != NULL)
+        argc++;
     exit(main(argc, argv));
 }
