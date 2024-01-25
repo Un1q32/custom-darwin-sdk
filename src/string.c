bbd7c4b4fae1369a92d2e11beeed96bbe2df8405
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-24 22:41:42 -0500

    remove always false statement

--- src/string.c
+++ src/string.c
@@ -64,10 +64,7 @@ int strcmp(const char *s1, const char *s2) {
 int strncmp(const char *s1, const char *s2, size_t n) {
     while (n-- && *s1 && (*s1 == *s2))
         s1++, s2++;
-    if (n < 0)
-        return 0;
-    else
-        return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+    return *(const unsigned char *)s1 - *(const unsigned char *)s2;
 }
 
 char *strstr(const char *haystack, const char *needle) {
