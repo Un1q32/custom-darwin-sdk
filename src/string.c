1e759c3a1db4acf24d81f7c011b82c8b8e8964fb
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-24 23:29:13 -0500

    fix strncmp (fixes getenv)

--- src/string.c
+++ src/string.c
@@ -62,9 +62,12 @@ int strcmp(const char *s1, const char *s2) {
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-    while (n-- && *s1 && (*s1 == *s2))
-        s1++, s2++;
-    return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+    if (!n--)
+        return 0;
+    const unsigned char *s1_ = (void *)s1, *s2_ = (void *)s2;
+    while (*s1_ && *s2_ && n && *s1_ == *s2_)
+        s1_++, s2_++, n--;
+    return *s1_ - *s2_;
 }
 
 char *strstr(const char *haystack, const char *needle) {
