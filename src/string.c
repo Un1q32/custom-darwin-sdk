605f08ec5307aa83e911bae19a3a5acd0061b9e5
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-24 22:38:45 -0500

    more readable syntax

--- src/string.c
+++ src/string.c
@@ -64,7 +64,10 @@ int strcmp(const char *s1, const char *s2) {
 int strncmp(const char *s1, const char *s2, size_t n) {
     while (n-- && *s1 && (*s1 == *s2))
         s1++, s2++;
-    return n < 0 ? 0 : *(const unsigned char *)s1 - *(const unsigned char *)s2;
+    if (n < 0)
+        return 0;
+    else
+        return *(const unsigned char *)s1 - *(const unsigned char *)s2;
 }
 
 char *strstr(const char *haystack, const char *needle) {
