3ba2c74af4eddae3ee682699ceff151c51821f1a
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-25 23:14:34 -0500

    better system test

--- tests/system.c
+++ tests/system.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int main() { return system("ls -al"); }
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Usage: %s <command>\n", argv[0]);
+    return 1;
+  }
+  int ret = system(argv[1]);
+  printf("Return value: %d\n", ret);
+  return 0;
+}
