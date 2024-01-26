59b48acc737e6dcc5ecd38d348900f5b5029250b
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-25 22:39:56 -0500

    add wait, wait3, waitpid

--- include/sys/wait.h
+++ include/sys/wait.h
@@ -3,6 +3,9 @@
 
 #include <sys/types.h>
 
+extern pid_t wait(int *);
+extern pid_t wait3(int *, int, struct rusage *);
 extern pid_t wait4(pid_t, int *, int, struct rusage *);
+extern pid_t waitpid(pid_t, int *, int);
 
 #endif
---
+++ src/wait.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+pid_t waitpid(pid_t pid, int *status, int options) {
+  return wait4(pid, status, options, NULL);
+}
+
+pid_t wait3(int *status, int options, struct rusage *rusage) {
+  return wait4(-1, status, options, rusage);
+}
+
+pid_t wait(int *status) { return wait3(status, 0, NULL); }
