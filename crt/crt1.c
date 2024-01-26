bd47b6fa2ce993c8c218c31f9aeca8fd070b7e19
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-25 19:11:03 -0500

    work pls

--- crt/crt1.c
+++ crt/crt1.c
@@ -7,16 +7,28 @@ extern const char **NXArgv;
 extern const char **environ;
 extern const char *__progname;
 
+static const char *crt_basename(const char *path) {
+  const char *base = path;
+  while (*path != '\0') {
+    if (*path == '/')
+      base = path + 1;
+    path++;
+  }
+  return base;
+}
+
 void start(void) __asm__("start");
 void start(void) {
   int argc = 0;
   const char **argv = (const char **)(&argc + 4);
-  while (argv[argc] != NULL)
-    argc++;
+  int argc2 = 0;
+  while (argv[argc2] != NULL)
+    argc2++;
+  argc = argc2;
   const char **envp = argv + argc + 1;
   NXArgc = argc;
   NXArgv = argv;
   environ = envp;
-  __progname = ((argv[0] != NULL) ? basename((char *)argv[0]) : "");
+  __progname = ((argv[0] != NULL) ? crt_basename(argv[0]) : "");
   exit(main(argc, argv, envp));
 }
