1362fe812e233ae1c0d6bfc82b782e2763bcfdd2
OldWorldOrdr <joey.t.reinhart@gmail.com>
2024-01-26 22:48:16 -0500

    add struct tm

--- include/time.h
+++ include/time.h
@@ -3,6 +3,20 @@
 
 typedef long time_t;
 
+struct tm {
+  int tm_sec;
+  int tm_min;
+  int tm_hour;
+  int tm_mday;
+  int tm_mon;
+  int tm_year;
+  int tm_wday;
+  int tm_yday;
+  int tm_isdst;
+  long tm_gmtoff;
+  char *tm_zone;
+};
+
 struct timespec {
   time_t tv_sec;
   long tv_nsec;
