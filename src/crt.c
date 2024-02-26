1943c2b9e201687f7fbd42716db22eea06fa0745
Un1q32 <joey.t.reinhart@gmail.com>
2024-02-26 17:03:48 -0500

    combine crt_externs.c and entry.c

---
+++ src/crt.c
@@ -0,0 +1,22 @@
+#include <libgen.h>
+#include <stdlib.h>
+
+extern int main(int argc, char *argv[], char *envp[]);
+
+int NXArgc = 0;
+char **NXArgv = NULL;
+char **environ = NULL;
+char *__progname = NULL;
+
+int *_NSGetArgc(void) { return &NXArgc; }
+char ***_NSGetArgv(void) { return &NXArgv; }
+char ***_NSGetEnviron(void) { return &environ; }
+char **_NSGetProgname(void) { return &__progname; }
+
+void _start(int argc, char **argv, char **envp) {
+  NXArgc = argc;
+  NXArgv = argv;
+  environ = envp;
+  __progname = ((argv[0] != NULL) ? basename((char *)argv[0]) : "");
+  exit(main(argc, argv, envp));
+}
--- src/crt_externs.c
+++
@@ -1,9 +0,0 @@
-extern int NXArgc;
-extern char **NXArgv;
-extern char **environ;
-extern char *__progname;
-
-int *_NSGetArgc(void) { return &NXArgc; }
-char ***_NSGetArgv(void) { return &NXArgv; }
-char ***_NSGetEnviron(void) { return &environ; }
-char **_NSGetProgname(void) { return &__progname; }
