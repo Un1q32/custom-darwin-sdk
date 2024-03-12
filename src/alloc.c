3a0a1bc21582874880089e03682158a8ba766816
Un1q32 <joey.t.reinhart@gmail.com>
2024-03-12 16:56:48 -0400

    malloc and realloc shouldn't return NULL for size 0

--- src/alloc.c
+++ src/alloc.c
@@ -32,11 +32,6 @@ void *realloc(void *ptr, size_t size) {
   if (ptr == NULL)
     return malloc(size);
 
-  if (size == 0) {
-    free(ptr);
-    return NULL;
-  }
-
   void *new_ptr = malloc(size);
   if (new_ptr == NULL)
     return NULL;
