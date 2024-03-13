16a384cf58a20a8b159e99f8f1ac393fce635530
Un1q32 <joey.t.reinhart@gmail.com>
2024-03-13 16:42:15 -0400

    fix

--- make/i386.mk
+++ make/i386.mk
@@ -1,11 +1,14 @@
 ARCH := i386
-BUILTINS := udivdi3 umoddi3
+BUILTINS := udivdi3 umoddi3 fixunsxfdi
 _BUILTINS := $(addprefix src/$(ARCH)-,$(addsuffix .o,$(BUILTINS)))
 BCC = $(V)src=$@; src=$${src\#\#*/}; printf " \033[1;32mCC\033[0m %s\n" "$$src"; $(_BUILTIN_CC) $@ $<
 
 .PHONY: all
 
 all: $(_BUILTINS)
+
+src/$(ARCH)-fixunsxfdi.o: compiler-rt/lib/builtins/fixunsxfdi.c
+	$(BCC)
 
 ifdef NOASM
 
