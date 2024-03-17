a2fa5e851bff66d457525bf4250cb2ff61a92636
Un1q32 <joey.t.reinhart@gmail.com>
2024-03-17 18:19:19 -0400

    move some kernel magic stuff to sys

--- include/poll.h
+++ include/poll.h
@@ -1,19 +1,7 @@
 #ifndef _POLL_H_
 #define _POLL_H_
 
-#define POLLIN 0x0001
-#define POLLOUT 0x0004
-#define POLLERR 0x0008
-#define POLLHUP 0x0010
-#define POLLNVAL 0x0020
-
-typedef unsigned int nfds_t;
-
-struct pollfd {
-  int fd;
-  short events;
-  short revents;
-};
+#include <sys/poll.h>
 
 extern int poll(struct pollfd[], nfds_t, int);
 
--- include/termios.h
+++ include/termios.h
@@ -1,32 +1,7 @@
 #ifndef _TERMIOS_H_
 #define _TERMIOS_H_
 
-#define VMIN 16
-#define VTIME 17
-#define INPCK 0x00000010
-#define ISTRIP 0x00000020
-#define ICRNL 0x00000100
-#define IXON 0x00000200
-#define OPOST 0x00000001
-#define CS8 0x00000300
-#define TCSANOW 0
-#define TCSAFLUSH 2
-#define IGNBRK 0x00000001
-#define BRKINT 0x00000002
-#define ISIG 0x00000080
-#define ICANON 0x00000100
-#define IEXTEN 0x00000400
-#define ECHO 0x00000008
-
-struct termios {
-  unsigned long c_iflag;
-  unsigned long c_oflag;
-  unsigned long c_cflag;
-  unsigned long c_lflag;
-  unsigned char c_cc[20];
-  unsigned long c_ispeed;
-  unsigned long c_ospeed;
-};
+#include <sys/termios.h>
 
 extern int tcgetattr(int, struct termios *);
 extern int tcsetattr(int, int, const struct termios *);
