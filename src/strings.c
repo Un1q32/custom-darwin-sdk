fbe988ad0674e233fcb30426913d512587cf6af9
Un1q32 <joey.t.reinhart@gmail.com>
2024-03-19 02:41:05 -0400

    improve bzero

--- src/strings.c
+++ src/strings.c
@@ -2,11 +2,9 @@
 
 int bcmp(const void *s1, const void *s2, size_t n) { return memcmp(s1, s2, n); }
 
-void __bzero(void *s, size_t n) {
-  char *buf = s;
-  size_t i;
-  for (i = 0; i <= n; i++)
-    *buf++ = 0;
+void __bzero(void *buf, size_t n) {
+  while (n--)
+    *(char *)buf++ = 0;
 }
 
 void bzero(void *s, size_t n) { __bzero(s, n); }
