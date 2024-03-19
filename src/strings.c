adbc0a75bfdbd13a01674eb04615c232fdcf0c32
Un1q32 <joey.t.reinhart@gmail.com>
2024-03-19 15:11:39 -0400

    fix

--- src/strings.c
+++ src/strings.c
@@ -3,7 +3,7 @@
 int bcmp(const void *s1, const void *s2, size_t n) { return memcmp(s1, s2, n); }
 
 void __bzero(void *buf, size_t n) {
-  char *p = buf
+  char *p = buf;
   while (n--)
     *p++ = 0;
 }
