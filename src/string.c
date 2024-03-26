3eb2fe0a53f4c60bff8a773bdc99dc08ed20bdb8
Un1q32 <joey.t.reinhart@gmail.com>
2024-03-25 22:36:36 -0400

    finish strerror

--- src/string.c
+++ src/string.c
@@ -379,6 +379,108 @@ char *strerror(int errnum) {
     return "Connection reset by peer";
   case ENOBUFS:
     return "No buffer space available";
+  case EISCONN:
+    return "Socket is already connected";
+  case ENOTCONN:
+    return "Socket is not connected";
+  case ESHUTDOWN:
+    return "Cannot send after socket shutdown";
+  case ETOOMANYREFS:
+    return "Too many references: cannot splice";
+  case ETIMEDOUT:
+    return "Connection timed out";
+  case ECONNREFUSED:
+    return "Connection refused";
+  case ELOOP:
+    return "Too many levels of symbolic links";
+  case ENAMETOOLONG:
+    return "File name too long";
+  case EHOSTDOWN:
+    return "Host is down";
+  case EHOSTUNREACH:
+    return "No route to host";
+  case ENOTEMPTY:
+    return "Directory not empty";
+  case EPROCLIM:
+    return "Too many processes";
+  case EUSERS:
+    return "Too many users";
+  case EDQUOT:
+    return "Disc quota exceeded";
+  case ESTALE:
+    return "Stale NFS file handle";
+  case EREMOTE:
+    return "Too many levels of remote in path";
+  case EBADRPC:
+    return "RPC struct is bad";
+  case ERPCMISMATCH:
+    return "RPC version wrong";
+  case EPROGUNAVAIL:
+    return "RPC prog. not avail";
+  case EPROGMISMATCH:
+    return "Program version wrong";
+  case EPROCUNAVAIL:
+    return "Bad procedure for program";
+  case ENOLCK:
+    return "No locks available";
+  case ENOSYS:
+    return "Function not implemented";
+  case EFTYPE:
+    return "Inappropriate file type or format";
+  case EAUTH:
+    return "Authentication error";
+  case ENEEDAUTH:
+    return "Need authenticator";
+  case EPWROFF:
+    return "Device power is off";
+  case EDEVERR:
+    return "Device error";
+  case EOVERFLOW:
+    return "Value too large to be stored in data type";
+  case EBADEXEC:
+    return "Bad executable";
+  case EBADARCH:
+    return "Bad CPU type in executable";
+  case ESHLIBVERS:
+    return "Shared library version mismatch";
+  case EBADMACHO:
+    return "Malformed Macho file";
+  case ECANCELED:
+    return "Operation canceled";
+  case EIDRM:
+    return "Identifier removed";
+  case ENOMSG:
+    return "No message of desired type";
+  case EILSEQ:
+    return "Illegal byte sequence";
+  case ENOATTR:
+    return "Attribute not found";
+  case EBADMSG:
+    return "Bad message";
+  case EMULTIHOP:
+    return "Reserved";
+  case ENODATA:
+    return "No message available on STREAM";
+  case ENOLINK:
+    return "Reserved";
+  case ENOSR:
+    return "No STREAM resources";
+  case ENOSTR:
+    return "Not a STREAM";
+  case EPROTO:
+    return "Protocol error";
+  case ETIME:
+    return "STREAM ioctl timeout";
+  case EOPNOTSUPP:
+    return "Operation not supported on socket";
+  case ENOPOLICY:
+    return "No such policy registered";
+  case ENOTRECOVERABLE:
+    return "State not recoverable";
+  case EOWNERDEAD:
+    return "Previous owner died";
+  case EQFULL:
+    return "Interface output queue is full";
   default:
     return "Unknown error";
   }
